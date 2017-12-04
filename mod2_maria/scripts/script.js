71ce47d5f6caaec0459162b5e3bf4dc146f57584
Maria Pyshna <lunarocket@mail.ru>
2017-12-04 15:06:55 +0100

    Styled senate page

--- mod2_maria/scripts/script.js
+++ mod2_maria/scripts/script.js
@@ -37,3 +37,103 @@ function buildTable() {
 
     })
 }
+
+
+
+
+
+var dataHandler = new DataHandler();
+dataHandler.init();
+dataHandler.loadJsonFile();
+
+console.log(dataHandler)
+
+
+$(document).on("ready", function() {
+    console.log("document loaded...")
+})
+
+// $(document).on("click", function(){
+// 	console.log("document clicked...")
+// } )
+
+$("#myTextInput").on("change", function() {
+    console.log("change...");
+    $("#textValue").text($(this).val())
+})
+
+$("#myTextInput").on("focus", function() {
+    console.log("focus...");
+    $(this).css("background", "red");
+})
+
+$("#myTextInput").on("blur", function() {
+    console.log("blur...");
+    $(this).css("background", "white");
+})
+
+$("#myTextInput").on("blur", function() {
+    console.log("blur...");
+    $(this).css("background", "white");
+})
+
+
+$("#myTextInput").on("keydown", function() {
+    console.log("keydown...");
+
+})
+$("#myTextInput").on("keyup", function() {
+    console.log("keyup...");
+    $("#textValue").text($(this).val())
+})
+$("#myTextInput").on("keypress", function() {
+    console.log("keypress...");
+})
+
+
+$("#filter input[type=checkbox]").on("change",function(){
+	console.log("checkbox change");
+
+	tgifFilter.updateParty( $(this).val(), $(this).prop("checked") )
+})
+
+
+// $("#myTextInput").change(function(){
+// 	console.log("change with _change_")
+// })
+
+
+
+function Filter (){
+
+	this.D = true;
+	this.R = true;
+	this.I = true;
+
+	this.activeState = "";
+
+	this.updateParty = function(party, checked) {
+		console.log("should update parties now...", party, checked)
+
+
+		// if(party == "D"){
+		// 	if(checked){
+		// 		this.D = true
+		// 	} else{
+		// 		this.D = false
+		// 	}
+		// }
+
+		// ..better
+		this[party] = checked;
+
+
+
+		dataHandler.updateTable();
+
+	}
+
+}
+var tgifFilter = new Filter();
+
+console.log(tgifFilter);
